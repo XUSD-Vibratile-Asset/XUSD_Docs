78007566eb831f0c5422035e1c0f8bae45a07b21
Surfacing8671 <jaeweb8671@gmail.com>
2024-10-24 04:40:33 -0500

    Update navbar.tsx

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -68,7 +68,7 @@ export function Navbar() {
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2.5">
-      <Image src={"/logoMain.png"} width={100} height={100} alt="mooo"></Image>
+      <Image src={"/logoMain.png"} width={60} height={60} alt="mooo"></Image>
       <h2 className="text-md font-bold">XUSD Docs</h2>
     </Link>
   );
